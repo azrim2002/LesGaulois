43ad6bfafcbd0d7768ed28bdfef108edf0666bd9
azrim2002 <79055744+azrim2002@users.noreply.github.com>
2022-10-12 19:59:36 +0200

    LesGaulois

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,32 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force ;
+	private int effetPotion=1 ;
+	
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setEffetPotion(int effetPotion) {
+		this.effetPotion = effetPotion;
+	}
+
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	
+
+}
